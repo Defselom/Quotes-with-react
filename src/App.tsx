0a4b3d755df959b47646d45cfd0e774479247c35
defselom <selomadzomla@gmail.com>
2023-12-20 08:26:52 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="text-red-400">
+        <h1>Quote project </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
